7144d70b812ad1a0967203c9a4219b7337bc0834
Michael Spencer <sonrisesoftware@gmail.com>
2016-09-09 22:58:51 -0500

    Fish config support for macOS

--- config.fish
+++ config.fish
@@ -1,11 +1,16 @@
-set python3_root ~/.local/lib/python3.5/site-packages
+set python3_root (pip3 show powerline-status | grep Location | cut -d':' -f2 | string trim)
 
 set fish_function_path $fish_function_path "$python3_root/powerline/bindings/fish"
 powerline-setup
 
-eval (python -m virtualfish)
+eval (python3 -m virtualfish)
 
 # set -x PATH (ruby -e 'print Gem.user_dir')/bin $PATH
 
-set -x PATH $PATH ~/.local/bin
-set -x QML2_IMPORT_PATH ~/.local/lib/qt/qml/ $QML2_IMPORT_PATH
+switch (uname)
+case Darwin
+    set -x PATH $PATH ~/Library/bin
+case Linux
+    set -x PATH $PATH ~/.local/bin
+    set -x QML2_IMPORT_PATH ~/.local/lib/qt/qml/ $QML2_IMPORT_PATH
+end
