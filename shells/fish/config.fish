009882b8367e3cacb3032a828c66617c10fee7a7
Michael Spencer <sonrisesoftware@gmail.com>
2017-09-19 19:16:39 -0500

    fish: Updated config

--- shells/fish/config.fish
+++ shells/fish/config.fish
@@ -1,16 +1,30 @@
-set python3_root (pip3 show powerline-status | grep Location | cut -d':' -f2 | string trim)
+if command --search powerline >/dev/null
+    set python3_root (python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
+    #set python3_root (pip3 show powerline-status | grep Location | cut -d':' -f2 | string trim)
+    set fish_function_path $fish_function_path "$python3_root/powerline/bindings/fish"
+    powerline-setup
+    powerline-daemon -q
+end
 
-set fish_function_path $fish_function_path "$python3_root/powerline/bindings/fish"
-powerline-setup
+if command --search yarn >/dev/null
+    set -x PATH (yarn global bin --offline) $PATH
+end
 
-eval (python3 -m virtualfish)
-
-# set -x PATH (ruby -e 'print Gem.user_dir')/bin $PATH
-set -x PATH (yarn global bin) $PATH
+if command --search ruby > /dev/null
+    set -x PATH (ruby -e 'print Gem.user_dir')/bin $PATH
+end
 
 if test -d ~/.cargo/bin
     set -x PATH $PATH ~/.cargo/bin
 end
+
+if test -d ~/.local/bin
+    set -x PATH $PATH ~/.local/bin
+end
+
+eval (python3 -m virtualfish)
+
+set -x LEDGER_FILE ~/Documents/personal/finances/current.ledger
 
 switch (uname)
 case Darwin
@@ -19,11 +33,6 @@ case Darwin
     alias dock-add-spacer "defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type=\"spacer-tile\";}'; and killall Dock"
     alias show-files 'defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
     alias hide-files 'defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
-
-    # On Linux, there's a systemd service for this
-    powerline-daemon -q
 case Linux
-    set -x PATH $PATH ~/.local/bin
-    set -x QML2_IMPORT_PATH ~/.local/lib/qt/qml/ $QML2_IMPORT_PATH
     set -x ANSIBLE_ROLES_PATH ~/.local/share/ansible/roles
 end
